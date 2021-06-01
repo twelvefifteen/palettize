757681ba1b97396ad4a1d84cf0b6946b9de1a66e
gvelasquez <54555927+twelvefifteen@users.noreply.github.com>
2021-05-31 21:18:41 -0500

    Updated todo.txt; code now resides in 'src' and is built into 'dist'

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 
 if not "%Platform%" == "X64" if not "%Platform%" == "x64" call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
-set WarningsFlags=-Wall -WX -wd4062 -wd4514 -wd4710 -wd4711 -wd4820 -wd5045
+set WarningsFlags=-W4 -WX -wd4062 -wd4514 -wd4710 -wd4711 -wd4820 -wd5045
 
 set CompilerFlags=-arch:AVX2 -EHa- -FC -fp:except- -fp:fast -Gm- -GR- -MT -nologo -O2 -Oi -Zo -Z7
 REM Set PALETTIZE_CIELAB to zero to use sRGB colors instead
@@ -10,4 +10,9 @@ set CompilerFlags=-DPALETTIZE_CIELAB=1 -DPALETTIZE_DEBUG=0 -D_CRT_SECURE_NO_WARN
 
 set LinkerFlags=-incremental:no -opt:ref
 
-cl %WarningsFlags% %CompilerFlags% palettize.cpp /link %LinkerFlags%
+if not exist dist mkdir dist
+pushd dist
+
+cl %WarningsFlags% %CompilerFlags% ..\src\palettize.cpp /link %LinkerFlags%
+
+popd
